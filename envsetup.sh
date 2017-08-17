7125e9fdc3ae0da227bbe919e4a1d6086a3c21fd
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add Custom build support
    core: Move aosp sepolicy to device/
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

--- envsetup.sh
+++ envsetup.sh
@@ -137,6 +137,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^pearl_") ; then
+        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^pearl_//g')
+        export BUILD_NUMBER=$( (date +%s%N ; echo $CUSTOM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
+    else
+        CUSTOM_BUILD=
+    fi
+    export CUSTOM_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -623,6 +631,8 @@ function lunch()
         echo "Invalid lunch combo: $selection"
         return 1
     fi
+
+    check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
