e2150d9f65da9b4cc9e68d50f242f06937fac43a
LuK1337 <priv.luk@gmail.com>
2018-12-05 20:51:49 +0100

    build: Support system-as-root A-only backuptool
    Change-Id: Ia24ae288a48721f724bfebadfe023330c4932633

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -840,9 +840,14 @@ script.AppendExtra("sleep (2);")
   script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0755, 0755, None, None)
 
   if OPTIONS.backuptool:
+    is_system_as_root = script.fstab["/system"].mount_point == "/"
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/system"
     script.Mount("/system")
-    script.RunBackup("backup")
+    script.RunBackup("backup", "/system/system" if is_system_as_root else "/system")
     script.Unmount("/system")
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/"
 
   script.Mount("/system")
   script.RunCleanCache()
@@ -917,9 +922,14 @@ script.AppendExtra("sleep (2);")
 
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
+    is_system_as_root = script.fstab["/system"].mount_point == "/"
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/system"
     script.Mount("/system")
-    script.RunBackup("restore")
+    script.RunBackup("restore", "/system/system" if is_system_as_root else "/system")
     script.Unmount("/system")
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/"
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
